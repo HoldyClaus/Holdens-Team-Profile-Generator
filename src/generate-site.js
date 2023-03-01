97e5b3a62d8e406547682d39e9e69234c1a5ea82
HoldyClaus <holdenclaus0212@gmail.com>
2023-02-28 19:52:20 -0600

    slight changes

--- src/generate-site.js
+++ src/generate-site.js
@@ -1,3 +1,8 @@
+const { Employee } = require("../lib");
+const { Manager } = require("../lib");
+const { Engineer } = require("../lib");
+const { Intern } = require("../lib");
+
 function createTeam(theTeam){
 
 
@@ -69,7 +74,7 @@ function createTeam(theTeam){
             <title>My Team</title>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
                 integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-            <link rel="stylesheet" href="style.css">
+            <link rel="stylesheet" href="../dist/style.css">
             <script src="https://kit.fontawesome.com/c502137733.js"></script>
         </head>
         <body>
